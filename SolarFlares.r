16e6b9d22de742b8de539e14c34290d5a30fbe1c
Tyler-Balka <115902729+Tyler-Balka@users.noreply.github.com>
2023-10-26 10:58:22 -0500

    finished subtask 3 + 4

--- SolarFlares.r
+++ SolarFlares.r
@@ -20,72 +20,97 @@ filtered_data <- SolarData %>%
 filter(x.pos.asec >= (locationX - 10), x.pos.asec <= (locationX + 10))
 plot(SolarData$x.pos.asec, SolarData$y.pos.asec)
 
+#############################################################################
+#install.packages("readxl")
+library(readxl)
 
-####################################### steps to creating method 1 according to ChatGPT ###############################################
-
-# Creating Method 1 for Solar Flare Intensity Estimation based on the total.counts attribute involves a series of steps. This method aims to estimate the intensity of solar flares in a specific location based on the total counts recorded for flare events. Here are the steps to implement Method 1:
-
-# Step 1: Data Preparation
-# 1.1. Collect the solar flare dataset for Set 1 (2004-2005).
-setwd("G:/My Drive/UH Fall 2023/COSC 3337 Data Science I/Group Project/CODE")
-setwd("C:/Users/Tyler/OneDrive/Documents/Data Science I/Group Project/Dataset")
-SolarData <- read.csv("Solar_flare_RHESSI_2004_05.csv")
-
-# 1.2. Filter the data to include only relevant attributes for this method: 'total.counts', 'X pos [asec]', and 'Y pos [asec]'.
-filtered_data = subset(SolarData, select = c(total.counts, x.pos.asec, y.pos.asec))
-
-# Step 2: Spatial Grid Creation
-# 2.1. Define a spatial grid to represent the region of interest (e.g., the solar disk). The grid cells should align with the X and Y positions.
-cell_size = 10
-
-grid = expand.grid(
-    X = seq(min(SolarData$x.pos.asec), max(SolarData$x.pos.asec), by = cell_size),
-    Y = seq(min(SolarData$y.pos.asec), max(SolarData$y.pos.asec), by = cell_size)
-)
-
-# 2.2. Calculate the dimensions and resolution of the grid cells based on the solar flare data.
-num_rows = ceiling((max(SolarData$y.pos.asec) - min(SolarData$y.pos.asec)) / cell_size)
-num_cols = ceiling((max(SolarData$x.pos.asec) - min(SolarData$x.pos.asec)) / cell_size)
-
-# Step 3: Aggregating Data
-# 3.1. Group the flare events by their X and Y positions and assign them to the corresponding grid cells.
-library(dplyr)
-grouped_data = SolarData %>% group_by(
-                            X = cut(x.pos.asec, breaks=unique(grid$X)),
-                            Y = cut(y.pos.asec, breaks=unique(grid$Y))) %>% 
-            summarize(Total_Counts=sum(total.counts))
+#subtask for task 1
 
 
-# 3.2. In each grid cell, calculate the total count of all flare events falling within that cell.
+#1.
+#Method 1
+########################################################################################################################################
 
-# Step 4: Intensity Estimation
-# 4.1. For each grid cell, calculate the intensity as the total count of flare events within the cell.
-grouped_data$Intensity = grouped_data$Total_Counts
+# Set the path to your xlsx file
+file_path <- "/Users/miguelgarcia/Desktop/University of Houston/Fall 2023/Data Science 1 3337/Group_Project/Solar_flare_RHESSI_2004_05.xlsx"
 
-# 4.2. You can also normalize the intensity values to compare the relative intensity in different regions of the solar disk.
-grouped_data$Normalized_Intensity = (grouped_data$Intensity - min(grouped_data$Intensity)) / (max(grouped_data$Intensity) - min(grouped_data$Intensity))
+# Read the xlsx file
+data_solar_flare_2004 <- read_excel(file_path)
 
-# Step 5: Visualization
-# 5.1. Create visualizations of the intensity map, with grid cells color-coded to represent the intensity level.
-# 5.2. You can use heatmaps or contour plots to visualize the intensity across the solar disk.
-library(ggplot2)
 
-ggplot(grouped_data, aes(x = X, y= Y, fill = Intensity)) +
+# Group by x.pos.asec and y.pos.asec and calculate the sum of total.counts for each location
+intensity_by_location_method1 <- aggregate(data_solar_flare_2004$total.counts, 
+                                   by=list(X=data_solar_flare_2004$x.pos.asec, Y=data_solar_flare_2004$y.pos.asec), sum)
+
+# Rename the columns
+colnames(intensity_by_location_method1) <- c("X", "Y", "Total_Intensity")
+
+
+########################################################################################################################################
+
+
+
+#2
+#Method 2
+########################################################################################################################################
+
+
+# Compute a derived intensity value by multiplying duration.s and energy.kev
+data_solar_flare_2004$derived_intensity <- data_solar_flare_2004$duration.s * as.numeric(as.character(data_solar_flare_2004$energy.kev))
+
+# Group by x.pos.asec and y.pos.asec and calculate the sum of derived_intensity for each location
+intensity_by_location_method2 <- aggregate(data_solar_flare_2004$derived_intensity, 
+                                           by=list(X=data_solar_flare_2004$x.pos.asec, Y=data_solar_flare_2004$y.pos.asec), sum)
+
+# Rename the columns
+colnames(intensity_by_location_method2) <- c("X", "Y", "Derived_Intensity")
+
+
+########################################################################################################################################
+
+# subtask 3 - create intensity maps for months 1+2+3+4 for method 1 and method 2
+
+# find months 1+2+3+4
+filtered_by_months_1_4 = subset(intensity_by_location_method1, month %in% 1:4)
+
+# create intensity map for method 1
+ggplot(filtered_by_months_1_4, aes(x=X, y=Y, fill=Total_Intensity)) +
     geom_tile() +
-    scale_fill_gradient(low = "white", high = "red") +
-    labs(title = "Solar Flare Intensity Map", x = "X Position", y = "Y Position") +
+    scale_fill_gradient(low = "white", high = "red") + 
+    labs(title= "Intensity Map for Method 1", x = "X Position", y = "Y Position") +
     theme_minimal()
 
-# Step 6: Analysis
-# 6.1. Analyze the intensity map to identify regions with high solar flare intensity.
-# 6.2. Compare the results with other datasets or solar activity information to draw conclusions about solar flare hotspots.
+# use intensity by location method 2
+filtered_by_months_1_4 = subset(intensity_by_location_method2, month %in% 1:4)
 
-# Step 7: Validation and Testing
-# 7.1. Validate the method's performance using known solar flare events.
-# 7.2. Test the method on different time periods or datasets to ensure its generalizability.
+  # create intensity map for method 2
+ggplot(filtered_by_months_1_4, aes(x=X, y=Y, fill="Derived_Intensity")) +
+    geom_tile() +
+    scale_fill_gradient(low="white", high="red") +
+    labs(title= "Intensity Map for Method 2", x= "X Position", y= "Y Position") +
+    theme_minimal()  
 
-# Step 8: Documentation and Reporting
-# 8.1. Document the method, including the mathematical formulation for intensity estimation.
-# 8.2. Prepare a report summarizing the findings, including the identified high-intensity regions and any insights gained from the analysis.
 
-# By following these steps, you can create Method 1 for solar flare intensity estimation based on the total.counts attribute. This method will help you generate intensity maps that highlight regions of high solar flare activity on the Sun's surface during the specified time period.
+##############################################################################################
+# subtask 4 - create intensity maps for months 21+22+23+24
+
+# method 1
+# find months 21+22+23+24
+filtered_by_months_21_24 = subset(intensity_by_location_method1, month %in% 21:24)
+
+# create intensity map for method 1
+ggplot(filtered_by_months_21_24, aes(x=X, y=Y, fill=Total_Intensity)) +
+    geom_tile() +
+    scale_fill_gradient(low = "white", high = "red") + 
+    labs(title= "Intensity Map for Method 1", x = "X Position", y = "Y Position") +
+    theme_minimal()
+
+# method 2
+filtered_by_months_21_24 = subset(intensity_by_location_method2, month %in% 21:24)
+
+# create intensity map for method 2
+ggplot(filtered_by_months_21_24, aes(x=X, y=Y, fill=Total_Intensity)) +
+    geom_tile() +
+    scale_fill_gradient(low = "white", high = "red") + 
+    labs(title= "Intensity Map for Method 1", x = "X Position", y = "Y Position") +
+    theme_minimal()
