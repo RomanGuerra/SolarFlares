57dd856abf46811434bfb04513ee993728c9733c
Tyler-Balka <115902729+Tyler-Balka@users.noreply.github.com>
2023-11-10 21:30:06 -0600

    started on time series

--- SolarFlares.r
+++ SolarFlares.r
@@ -1,7 +1,9 @@
 # Libraries
-# install.packages("readxl")
-# install.packages("lubridate")
-# install.packages("hexbin")
+install.packages("readxl")
+install.packages("lubridate")
+install.packages("hexbin")
+install.packages("ggthemes")
+install.packages("openxlsx")
 library(hexbin)
 library(RColorBrewer)
 library(lubridate)
@@ -9,14 +11,16 @@ library(readxl)
 library(dplyr)
 library(ggplot2)
 library(ggthemes)
+library(openxlsx)
+library(reshape2)
+
+
 
 # Set directory (change this to your directory)
-setwd("G:/My Drive/UH Fall 2023/COSC 3337 Data Science I/Group Project/CODE")
-# setwd("C:/Users/Tyler/OneDrive/Documents/Data Science I/Group Project/Dataset")
+setwd("C:/Users/Tyler/OneDrive/Documents/Data Science I/Group Project/Dataset")
 
 # Read File
 SolarData <- read.csv("Solar_flare_RHESSI_2004_05.csv")
-
 # Add Date column with class Date
 SolarData <- SolarData %>% mutate(Date = as.Date(paste(year, month, day, sep = "-")))
 
@@ -52,7 +56,7 @@ for (i in 1:11) {
   batch_start_date <- start_date %m+% months(overlap * (i - 1))
   batch_end_date <- batch_start_date %m+% months(batch_size) - days(1)
   
-
+  
   # Filter the data for the current batch
   batch_data <- SolarData %>%
     filter(Date >= batch_start_date, Date <= batch_end_date)
@@ -65,13 +69,13 @@ for (i in 1:11) {
                                              by=list(X=SolarData$x.pos.asec, Y=SolarData$y.pos.asec), sum)
   colnames(intensity_by_location_method1) <- c("x", "y", "total_intensity")
   
-    # Create a heat map for the current batch
+  # Create a heat map for the current batch
   if (i == 1 || i == 11) {
     intesity_map <- ggplot(method1_intensity, aes(x = `x.pos.asec`, y = `y.pos.asec`)) +
       geom_hex() + scale_fill_continuous(type = "viridis") +
       labs(title = paste("Solar Flare Intensity by Total Counts - Batch", i, "Heat Map"), x = "X Position", y = "Y Position", fill = "Intensity", subtitle = "using group") + theme_tufte()
     print(intesity_map)
-
+    
     intesity_map2 <- ggplot(intensity_by_location_method1, aes(x = `x`, y = `y`)) +
       geom_hex() + scale_fill_continuous(type = "viridis") +
       labs(title = paste("Solar Flare Intensity by Total Counts - Batch", i, "Heat Map"), x = "X Position", y = "Y Position", fill = "Intensity", subtitle = "using aggregate") + theme_tufte()
@@ -110,25 +114,112 @@ for (i in 1:11) {
     print(intesity_map4)
   }
 }
+#convert method1_intensities into a matrix In order for our function find hotspots to work
 
-# Task 2 - Part A - Hotspot Discovery Algorithm
-install.packages("sp")
-library(sp)
+##################CHECKKKK#############################
 
-# not sure if these would be correct values for d1 and d2
-# just looked at the plots and chose an arbitrary value
-d1 <- 400
-d2 <- 200
 
-# identify hotspots
-discover_hotspots <- function(intensity_map, threshold) {
-  # create contour lines at the given threshold
-  contour_lines <- contourLines(intensity_map, levels = threshold)
 
-  # create polygons based off the contour lines
-  polygons <- lapply(contour_lines, function(line){
-    polygon(coords = line$points, hole = FALSE)
-  })
 
-  hotspot_polygons <- SpatialPolygons(polygons)
+# Assuming that method1_intensity is already grouped and summarized
+
+# Get the range of coordinates
+x_range <- range(method1_intensity$`x.pos.asec`, na.rm = TRUE)
+y_range <- range(method1_intensity$`y.pos.asec`, na.rm = TRUE)
+
+# Create a sequence of x and y coordinates that cover the full range
+x_coords <- seq(from = min(x_range), to = max(x_range), by = 1)
+y_coords <- seq(from = min(y_range), to = max(y_range), by = 1)
+
+# Initialize a matrix to store the intensity values
+intensity_map <- matrix(0, nrow = length(y_coords), ncol = length(x_coords), 
+                        dimnames = list(y_coords, x_coords))
+
+
+
+
+
+
+
+# Fill the intensity_map matrix with the total_intensity values
+for (i in 1:nrow(method1_intensity)) {
+  # Find the index in the matrix that corresponds to the x and y coordinates
+  x_index <- which(x_coords == method1_intensity$`x.pos.asec`[i])
+  y_index <- which(y_coords == method1_intensity$`y.pos.asec`[i])
+  
+  # Assign the total intensity to the correct position in the matrix
+  # Check if the indices are within the bounds of the matrix
+  if (length(x_index) == 1 && length(y_index) == 1) {
+    intensity_map[y_index, x_index] <- method1_intensity$`total_intensity`[i]
+  }
 }
+
+# Define thresholds
+d1 <- 400  # High intensity threshold
+d2 <- 200  # Medium intensity threshold
+
+# Function to find hotspots
+find_hotspots <- function(intensity_map, threshold) {
+  hotspots <- which(intensity_map > threshold, arr.ind = TRUE)
+  hotspots_df <- as.data.frame(hotspots, stringsAsFactors = FALSE)
+  colnames(hotspots_df) <- c("Y", "X")
+  hotspots_df$Intensity <- intensity_map[hotspots]
+  return(hotspots_df)
+}
+
+# Find hotspots for both thresholds
+hotspots_d1 <- find_hotspots(intensity_map, d1)
+hotspots_d2 <- find_hotspots(intensity_map, d2)
+
+# Function to visualize hotspots
+visualize_hotspots <- function(intensity_map, hotspots_df) {
+  intensity_df <- as.data.frame(melt(intensity_map))
+  colnames(intensity_df) <- c("X", "Y", "Intensity")
+  
+  # Create the plot
+  p <- ggplot(intensity_df, aes(x = X, y = Y, fill = Intensity)) +
+    geom_tile() +
+    scale_fill_viridis_c(option = "C") +
+    labs(title = "Intensity Map with Hotspots", x = "X Coordinate", y = "Y Coordinate") +
+    theme_minimal() +
+    theme(legend.position = "right")
+  
+  # Add the hotspots if any
+  if (nrow(hotspots_df) > 0) {
+    p <- p + geom_point(data = hotspots_df, aes(x = X, y = Y), color = "red", size = 2)
+  }
+  
+  # Print the plot
+  print(p)
+}
+
+visualize_hotspots(intensity_map, hotspots_d1)
+visualize_hotspots(intensity_map, hotspots_d2)
+
+################### Time Series ###########################
+
+# store hotspot information for each batch
+hotspot_information <- list()
+
+# iterate over the 11 batches
+for (i in 1:11) {
+  batch_start_date <- start_date %m+% months(overlap * (i - 1))
+  batch_end_date <- batch_start_date %m+% months(batch_size) - days(1)
+  
+  
+  # Filter the data for the current batch
+  batch_data <- SolarData %>%
+    filter(Date >= batch_start_date, Date <= batch_end_date)
+  
+  # find hotspot for threshold d1
+  hotspots <- find_hotspots(intensity_map, d1)
+  
+  # store the hotspot information to be plotted later
+  hotspot_information[i] <- hotspots
+}
+
+
+
+
+
+
