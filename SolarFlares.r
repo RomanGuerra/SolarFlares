95422023204bc3fffea1ea5048d100b5a02242a2
RomanGuerra <roman29g@gmail.com>
2023-11-11 03:27:43 -0600

    Cleaned up task 1, added animation code

--- SolarFlares.r
+++ SolarFlares.r
@@ -1,9 +1,11 @@
 # Libraries
-install.packages("readxl")
-install.packages("lubridate")
-install.packages("hexbin")
-install.packages("ggthemes")
-install.packages("openxlsx")
+# install.packages("readxl")
+# install.packages("lubridate")
+# install.packages("hexbin")
+# install.packages("ggthemes")
+# install.packages("openxlsx")
+# install.packages("ggdensity")
+# install.packages("gganimate")
 library(hexbin)
 library(RColorBrewer)
 library(lubridate)
@@ -13,37 +15,51 @@ library(ggplot2)
 library(ggthemes)
 library(openxlsx)
 library(reshape2)
-
-
+library(tibble)
+library(ggdensity)
+library(gganimate)
 
 # Set directory (change this to your directory)
-setwd("C:/Users/Tyler/OneDrive/Documents/Data Science I/Group Project/Dataset")
+setwd("G:/My Drive/UH Fall 2023/COSC 3337 Data Science I/Group Project/CODE")
+# setwd("C:/Users/Tyler/OneDrive/Documents/Data Science I/Group Project/Dataset")
 
 # Read File
 SolarData <- read.csv("Solar_flare_RHESSI_2004_05.csv")
+
 # Add Date column with class Date
 SolarData <- SolarData %>% mutate(Date = as.Date(paste(year, month, day, sep = "-")))
 
 # Factor energy.kev
 Energy_Levels <- c("6-12", "12-25", "25-50", "50-100", "100-300", "300-800", "800-7000", "7000-20000")
-SolarData$energy.kev.f = factor(SolarData$energy.kev, levels = unique(Energy_Levels), ordered = T)
+SolarData$energy.kev.fact = factor(SolarData$energy.kev, levels = unique(Energy_Levels), ordered = T)
 
-Exclude_Levels <- c("12-25", "25-50", "50-100", "100-300", "300-800", "800-7000", "7000-20000")
-SolarData$energy.kev.fex <- factor(SolarData$energy.kev, levels = Energy_Levels[!Energy_Levels %in% Exclude_Levels], ordered = T)
+# Initial Graph
+solar_map <- ggplot(SolarData, aes(x = `x.pos.asec`, y = `y.pos.asec`, color = energy.kev.fact)) +
+  labs(title = "Solar Flares", x = "X (arcseconds)", y = "Y (arcseconds)", color = "Energy (kilo-electronvolts)", subtitle = "2004 to 2005") +
+  geom_point() + scale_fill_continuous() + 
+  theme_tufte()
+print(solar_map)
 
-solar_map <- ggplot(SolarData, aes(x = `x.pos.asec`, y = `y.pos.asec`, color = energy.kev.f)) +
+# Exclude lower energies
+Exclude_Levels <- c("6-12")
+SolarData$energy.kev.exclude <- factor(SolarData$energy.kev, levels = Energy_Levels[!Energy_Levels %in% Exclude_Levels], ordered = T)
+solar_map <- ggplot(SolarData, aes(x = `x.pos.asec`, y = `y.pos.asec`, color = energy.kev.exclude)) +
+  labs(title = "Solar Flares", x = "X (arcseconds)", y = "Y (arcseconds)", color = "Energy (kilo-electronvolts)", subtitle = "2004 to 2005 (excluding 6-12 keV)") +
   geom_point() + scale_fill_continuous(type = "viridis") +
   theme_tufte()
 print(solar_map)
 
-solar_map <- ggplot(SolarData, aes(x = `x.pos.asec`, y = `y.pos.asec`, size = radial)) +
+Exclude_Levels <- c("6-12", "12-25")
+SolarData$energy.kev.exclude <- factor(SolarData$energy.kev, levels = Energy_Levels[!Energy_Levels %in% Exclude_Levels], ordered = T)
+solar_map <- ggplot(SolarData, aes(x = `x.pos.asec`, y = `y.pos.asec`, color = energy.kev.exclude)) +
+  labs(title = "Solar Flares", x = "X (arcseconds)", y = "Y (arcseconds)", color = "Energy (kilo-electronvolts)", subtitle = "2004 to 2005 (excluding 6-25 keV)") +
   geom_point() + scale_fill_continuous(type = "viridis") +
   theme_tufte()
 print(solar_map)
+
+
 
 ########################################################################################################################################
-
-
 # TASK 1
 ## Method 1 - Solar Flare Intensity Estimation (based on total.counts)
 
@@ -51,69 +67,93 @@ print(solar_map)
 batch_size <- 4  # 4 months per batch
 overlap <- 2     # 2-month overlap
 start_date <- as.Date("2004-01-01")
+cordinate_plane <- 1000
+bins_count <- 20
 
+# Method 1
 for (i in 1:11) {
   batch_start_date <- start_date %m+% months(overlap * (i - 1))
   batch_end_date <- batch_start_date %m+% months(batch_size) - days(1)
   
-  
+
   # Filter the data for the current batch
   batch_data <- SolarData %>%
     filter(Date >= batch_start_date, Date <= batch_end_date)
   
+  # Method 1
   method1_intensity <- batch_data %>%
     group_by(x.pos.asec, y.pos.asec) %>%
     summarise(total_intensity = sum(total.counts))
   
-  intensity_by_location_method1 <- aggregate(SolarData$total.counts, 
-                                             by=list(X=SolarData$x.pos.asec, Y=SolarData$y.pos.asec), sum)
-  colnames(intensity_by_location_method1) <- c("x", "y", "total_intensity")
-  
-  # Create a heat map for the current batch
+  # Create a heat map for the required batch
   if (i == 1 || i == 11) {
-    intesity_map <- ggplot(method1_intensity, aes(x = `x.pos.asec`, y = `y.pos.asec`)) +
-      geom_hex() + scale_fill_continuous(type = "viridis") +
-      labs(title = paste("Solar Flare Intensity by Total Counts - Batch", i, "Heat Map"), x = "X Position", y = "Y Position", fill = "Intensity", subtitle = "using group") + theme_tufte()
+    intesity_map <- ggplot(method1_intensity, aes(x = x.pos.asec, y = `y.pos.asec`)) +
+      labs(title = paste("Solar Flare Intensity by Total Counts - Batch", i), x = "X (arcseconds)", y = "Y (arcseconds)", fill = "Intensity", subtitle = "2004 - 2005") +
+      geom_hex(bins = bins_count) + scale_fill_continuous(type = "viridis") + coord_cartesian(xlim = c(-cordinate_plane, cordinate_plane), ylim = c(-cordinate_plane, cordinate_plane)) +
+      theme_tufte()
     print(intesity_map)
     
-    intesity_map2 <- ggplot(intensity_by_location_method1, aes(x = `x`, y = `y`)) +
-      geom_hex() + scale_fill_continuous(type = "viridis") +
-      labs(title = paste("Solar Flare Intensity by Total Counts - Batch", i, "Heat Map"), x = "X Position", y = "Y Position", fill = "Intensity", subtitle = "using aggregate") + theme_tufte()
-    print(intesity_map2)
+    # intesity_map <- ggplot(method1_intensity, aes(x = `x.pos.asec`, y = `y.pos.asec`)) + geom_point() +
+    #   labs(title = paste("Solar Flare - Batch", i), x = "X (arcseconds)", y = "Y (arcseconds)", fill = "Intensity", subtitle = "2004 - 2005") + 
+    #   scale_fill_continuous(type = "viridis") + coord_cartesian(xlim = c(-cordinate_plane, cordinate_plane), ylim = c(-cordinate_plane, cordinate_plane)) +
+    #   theme_tufte()
+    # print(intesity_map)
+    
+    intesity_map <- ggplot(method1_intensity, aes(x = `x.pos.asec`, y = `y.pos.asec`)) + stat_density_2d(aes(fill = ..density..), geom = "raster", contour = FALSE) +
+      labs(title = paste("Solar Flare - Batch", i), x = "X (arcseconds)", y = "Y (arcseconds)", fill = "Intensity", subtitle = "2004 - 2005") + 
+      scale_fill_continuous(type = "viridis", guide = "none") + coord_cartesian(xlim = c(-cordinate_plane, cordinate_plane), ylim = c(-cordinate_plane, cordinate_plane)) +
+      theme_tufte()
+    print(intesity_map)
   }
 }
 
-
+# Method 2
 for (i in 1:11) {
   batch_start_date <- start_date %m+% months(overlap * (i - 1))
   batch_end_date <- batch_start_date %m+% months(batch_size) - days(1)
-  
   
   # Filter the data for the current batch
   batch_data <- SolarData %>%
     filter(Date >= batch_start_date, Date <= batch_end_date)
   
+  # Method 2
   method2_intensity <- batch_data %>%
     group_by(x.pos.asec, y.pos.asec) %>%
-    summarise(total_duration = sum(duration.s))
+    summarise(total_intensity = sum((duration.s*.6 + energy.kev.f*.4)))
   
-  intensity_by_location_method2 <- aggregate(SolarData$duration.s, 
-                                             by=list(X=SolarData$x.pos.asec, Y=SolarData$y.pos.asec), sum)
-  colnames(intensity_by_location_method2) <- c("x", "y", "total_duration")
-  
+  # Create a heat map for the required batch
   if (i == 1 || i == 11) {
-    # Create a heat map for the current batch
-    intesity_map3 <- ggplot(method2_intensity, aes(x = `x.pos.asec`, y = `y.pos.asec`)) +
-      geom_hex() + scale_fill_continuous(type = "viridis") +
-      labs(title = paste("Solar Flare Intensity by Total Duration - Batch", i, "Heat Map"), x = "X Position", y = "Y Position", fill = "Intensity", subtitle = "using group") + theme_tufte()
-    print(intesity_map3)
-    
-    intesity_map4 <- ggplot(intensity_by_location_method2, aes(x = `x`, y = `y`)) +
-      geom_hex() + scale_fill_continuous(type = "viridis") +
-      labs(title = paste("Solar Flare Intensity by Total Duration - Batch", i, "Heat Map"), x = "X Position", y = "Y Position", fill = "Intensity", subtitle = "using aggregate") + theme_tufte()
+    intesity_map4 <- ggplot(method2_intensity, aes(x = `x.pos.asec`, y = `y.pos.asec`)) +
+      labs(title = paste("Solar Flare Intensity by Total Duration - Batch", i, "Heat Map"), x = "X (arcseconds)", y = "Y (arcseconds)", fill = "Intensity", subtitle = "2004 - 2005") + 
+      geom_hex() + scale_fill_continuous(type = "viridis") + coord_cartesian(xlim = c(-cordinate_plane, cordinate_plane), ylim = c(-cordinate_plane, cordinate_plane)) +
+      theme_tufte()
     print(intesity_map4)
   }
 }
+######################################################################################################################
+######################################################################################################################
+######################################################################################################################
+######################################################################################################################
+######################################################################################################################
+######################################################################################################################
+
+# Animation Code
+# need to install ffmpeg into your computer, it is not a library.
+p <- ggplot(SolarData, aes(x = x.pos.asec, y = `y.pos.asec`, color = energy.kev.fact)) + geom_point() +
+  theme_tufte() +
+  transition_time(month) + ease_aes('linear')
+
+animate(p, renderer = ffmpeg_renderer())
+anim_save("Video.mp4")
+
+######################################################################################################################
+######################################################################################################################
+######################################################################################################################
+######################################################################################################################
+######################################################################################################################
+######################################################################################################################
+
+
 #convert method1_intensities into a matrix In order for our function find hotspots to work
 
 ##################CHECKKKK#############################
